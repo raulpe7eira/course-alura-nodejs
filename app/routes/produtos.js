170da0494c24048dbecf6b60651d8587179dd580
Raul Pereira <raul.pereira@gmail.com>
2017-12-15 00:34:02 -0200

    Aula 05 - Respeitando o accept

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -18,11 +18,27 @@ module.exports = function (app) {
     });
 
     app.get('/produtos/form', function (req, res) {
-        res.render('produtos/form');
+        res.render('produtos/form', { errosValidacao: {}, produto: {} });
     });
 
     app.post('/produtos', function (req, res) {
         var produto = req.body;
+
+        req.assert('titulo', 'Titulo é obrigatório').notEmpty();
+        req.assert('preco', 'Formato inválido').isFloat();
+
+        var erros = req.validationErrors();
+        if (erros) {
+            res.format({
+                html: function () {
+                    res.status(400).render('produtos/form', { errosValidacao: erros, produto: produto });
+                },
+                json: function () {
+                    res.status(400).json(erros);
+                }
+            });
+            return;
+        }
 
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
